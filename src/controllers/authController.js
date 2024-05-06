f9d44916c07352117903e965286e28ca097247e4
Diana Mejías Hernández <85594834+dmejias08@users.noreply.github.com>
2024-05-06 00:05:32 -0600

    authentication service working

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,23 +7,82 @@ const sql = require("mssql");
 const router = express.Router();
 router.use(bodyParser.json());
 
-
+// SQL Server configuration
+var config = {
+  "user": "sqlserver", // Database username
+  "Password": "computines", // Database Password
+  "server": "34.69.60.15", // Server IP address
+  "database": "restec-db", // Database name
+  "options": {
+      "encrypt": false // Disable encryption
+  }
+}
 
 // Register a new user
 router.post('/register', async (req, res) => {
-  const { username, password } = req.body;
-  // Check if the username is already taken
-  if (users.find(user => user.username === username)) { // check if username exists in database
-    return res.status(statusCodes.FORBIDDEN).json({ message: 'Username already exists' });
-  }
-  try{
-    const hashedPassword = await bcrypt.hash(password, 10);
-    // Store the new user in the database
-    // Insert user into data base
-    // need to encode password
+  const { Id, Fullname, Rol, Password } = req.body;
+
+  console.log(Password)
+
+  try {
+    // Connect to the database
+    await sql.connect(config);
+
+    // Check if the username is already taken
+    const result = await sql.query`SELECT * FROM UserData WHERE Id = ${Id}`;
+    console.log("Result: " + result.recordset.length)
+    if (result.recordset.length > 0) {
+      return res.status(statusCodes.FORBIDDEN).json({ message: 'Id already exists' });
+    }
+
+    // Hash the Password
+    const hashedPassword = await bcrypt.hash(Password, 10);
+
+    console.log("Password: "+hashedPassword)
+
+    // Insert the new user into the database
+    await sql.query`INSERT INTO UserData (Id, Fullname, Rol, Password) VALUES (${Id}, ${Fullname}, ${Rol}, ${hashedPassword})`;
+
     res.status(statusCodes.CREATED).json({ message: 'User registered successfully' });
-  }catch{
+  } catch (error) {
+    console.error('Error registering user:', error);
     res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error' });
+  } finally {
+    // Close the connection
+    await sql.close();
+  }
+});
+
+// Authenticate user
+router.post('/login', async (req, res) => {
+  const { Id, Password } = req.body;
+
+  try {
+    // Connect to the database
+    await sql.connect(config);
+
+    // Retrieve user from the database
+    const result = await sql.query`SELECT * FROM UserData WHERE Id = ${Id}`;
+    const user = result.recordset[0];
+
+    if (!user) {
+      return res.status(statusCodes.FORBIDDEN).json({ message: 'Invalid username or Password' });
+    }
+
+    // Compare passwords
+    const passwordMatch = await bcrypt.compare(Password, user.Password);
+    if (!passwordMatch) {
+      return res.status(statusCodes.FORBIDDEN).json({ message: 'Invalid username or Password' });
+    }
+    console.log("UserId: "+user.Id);
+    console.log("UserName: "+user.Fullname);
+    res.status(statusCodes.OK).json(user);
+  } catch (error) {
+    console.error('Error authenticating user:', error);
+    res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error' });
+  } finally {
+    // Close the connection
+    await sql.close();
   }
 });
 
@@ -40,29 +99,6 @@ router.get("/user", (req, res) => {
   });
 });
 
-// Authenticate user
-router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-  // Find the user by username
-  const user = users.find(user => user.username === username);
-  // get username and password from database 
-  // need to encode password
-  // Check if the user exists and the password is correct
-  if (!user || user.password !== password) {
-    return res.status(statusCodes.FORBIDDEN).json({ message: 'Invalid username or password' });
-  }
-  try{
-    // Compare the provided password with the hashed password
-    const passwordMatch = await bcrypt.compare(password, user.password);
-    
-    if (!passwordMatch) {
-      return res.status(401).json({ message: 'Invalid username or password' });
-    }
-    // Authentication successful
-    res.status(statusCodes.CREATED).json({ message: 'Authentication successful' });
-  }catch{
-    res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error' });
-  }
-});
+
 
 module.exports = router;
