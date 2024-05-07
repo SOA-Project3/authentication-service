67791f265d16ae368ae0823c4d60996530c8bb38
Diana Mejías Hernández <85594834+dmejias08@users.noreply.github.com>
2024-05-06 23:49:42 -0600

    change insert method to avoid double connection

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,80 +3,77 @@ const bcrypt = require('bcrypt');
 const sql = require("mssql");
 
 
-
-// SQL Server configuration
-var config = {
-  "user": "sqlserver", // Database username
-  "Password": "computines", // Database Password
-  "server": "34.69.60.15", // Server IP address
-  "database": "restec-db", // Database name
-  "options": {
-      "encrypt": false // Disable encryption
-  }
-}
-
 // Register a new user
-
-const registerFunction = async (req, res) => {
+const registerFunction = (req, res) => {
   const { Id, Fullname, Rol, Password } = req.body;
 
+  // Check if the username is already taken
+  new sql.Request().query(`SELECT * FROM UserData WHERE Id = ${Id}`, async (err, result) => {
+    if (err) {
+      console.error("Error executing query:", err);
+      return res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error' });
+    }
 
-  try {
-    // Connect to the database
-    await sql.connect(config);
-
-    // Check if the username is already taken
-    const result = await sql.query`SELECT * FROM UserData WHERE Id = ${Id}`;
     if (result.recordset.length > 0) {
       return res.status(statusCodes.FORBIDDEN).json('Id already exists');
     }
 
-    // Hash the Password
-    const hashedPassword = await bcrypt.hash(Password, 10);
+    try {
+      // Hash the Password
+      const hashedPassword = await bcrypt.hash(Password, 10);
 
+      new sql.Request()
+      .input('Id', sql.Int, Id)
+      .input('Fullname', sql.NVarChar, Fullname)
+      .input('Rol', sql.NVarChar, Rol)
+      .input('Password', sql.NVarChar, hashedPassword)
+      .query('INSERT INTO UserData (Id, Fullname, Rol, Password) VALUES (@Id, @Fullname, @Rol, @Password)', (insertErr, insertResult) => {
+        if (insertErr) {
+          console.error("Error inserting user into database:", insertErr);
+          return res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error' });
+        }
 
-    // Insert the new user into the database
-    await sql.query`INSERT INTO UserData (Id, Fullname, Rol, Password) VALUES (${Id}, ${Fullname}, ${Rol}, ${hashedPassword})`;
-
-    res.status(statusCodes.CREATED).json({ message: 'User registered successfully' });
-  } catch (error) {
-    console.error('Error registering user:', error);
-    res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error' });
-  } finally {
-    // Close the connection
-    await sql.close();
-  }
+        console.log('User registered successfully');
+        res.status(statusCodes.CREATED).json({ message: 'User registered successfully' });
+      });
+    } catch (hashingError) {
+      console.error('Error hashing password:', hashingError);
+      return res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error' });
+    }
+  });
 };
 
-// Authenticate user
-const loginFunction = async (req, res) => {
+const loginFunction = (req, res) => {
   const { Id, Password } = req.body;
 
-  try {
-    // Connect to the database
-    await sql.connect(config);
+  // Execute the SQL query to retrieve user data
+  new sql.Request().query(`SELECT * FROM UserData WHERE Id = ${Id}`, (err, result) => {
+    if (err) {
+      console.error("Error executing query:", err);
+      return res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error' });
+    }
 
-    // Retrieve user from the database
-    const result = await sql.query`SELECT * FROM UserData WHERE Id = ${Id}`;
+    // Check if user exists
     const user = result.recordset[0];
-
     if (!user) {
-      return res.status(statusCodes.FORBIDDEN).json({ message: 'Invalid username or Password' });
+      return res.status(statusCodes.FORBIDDEN).json({ message: 'Invalid username or password' });
     }
 
     // Compare passwords
-    const passwordMatch = await bcrypt.compare(Password, user.Password);
-    if (!passwordMatch) {
-      return res.status(statusCodes.FORBIDDEN).json({ message: 'Invalid username or Password' });
-    }
-    res.status(statusCodes.OK).json(user);
-  } catch (error) {
-    console.error('Error authenticating user:', error);
-    res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error' });
-  } finally {
-    // Close the connection
-    await sql.close();
-  }
+    bcrypt.compare(Password, user.Password, (compareErr, passwordMatch) => {
+      if (compareErr) {
+        console.error("Error comparing passwords:", compareErr);
+        return res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error' });
+      }
+
+      if (!passwordMatch) {
+        return res.status(statusCodes.FORBIDDEN).json({ message: 'Invalid username or password' });
+      }
+
+      // Passwords match, return user data
+      res.status(statusCodes.OK).json(user);
+    });
+  });
 };
 
 
